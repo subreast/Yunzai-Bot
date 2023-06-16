23f0cf195d6cc22fd605145c47e548bfc4040d39
subreast <1556841@qq.com>
2023-06-16 08:15:32 +0800

    修改文案

--- plugins/example/入群帮助通知.js
+++ plugins/example/入群帮助通知.js
@@ -12,7 +12,7 @@ export class newcomer extends plugin {
 
   async accept() {
 
-    let msg = '欢迎使用Yunzai\n可发送【#帮助】【#喵喵帮助】【*帮助】【#图鉴帮助】【#群管帮助】【#土块帮助】查看各功能使用说明\n获取更多“私密”功能、反馈bug、反馈失效命令请加qq1556841联系'
+    let msg = '欢迎使用Yunzai\n可发送【#帮助】【#喵喵帮助】【*帮助】【#图鉴帮助】【#群管帮助】【#土块帮助】查看各功能使用说明\n反馈失效命令请加qq1556841联系\n绑定ck直接发送【#扫码登陆】，失效后可用命令【#刷新ck】自动刷新'
     if (this.e.user_id == Bot.uin) {
       await Bot.pickGroup(this.e.group_id).sendMsg(msg)
     }
